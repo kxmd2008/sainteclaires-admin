69a18757a341f09da392328c2399481bdd4adeca
kxmd2008 <kxmd2008@gmail.com>
2014-06-29 20:54:58 +0800

    update

--- src/main/java/org/luis/sainteclaires/admin/rest/AdminRest.java
+++ src/main/java/org/luis/sainteclaires/admin/rest/AdminRest.java
@@ -2,6 +2,9 @@ package org.luis.sainteclaires.admin.rest;
 
 import org.luis.basic.rest.model.SimpleMessage;
 import org.luis.basic.rest.model.SimpleMessageHead;
+import org.luis.sainteclaires.base.bean.Category;
+import org.luis.sainteclaires.base.bean.Product;
+import org.luis.sainteclaires.base.bean.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,14 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sainteclaires.base.bean.Category;
-import com.sainteclaires.base.bean.Product;
-import com.sainteclaires.base.bean.service.AccountService;
 
 @Controller
 @RequestMapping("/auth/")
 public class AdminRest {
-	@RequestMapping(value = "admin", method = RequestMethod.GET)
+	@RequestMapping("admin")
 	public String login() {
 		return "admin/login";
 	}
