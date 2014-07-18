af4baa682ced80501647beb6f05483f75701e1db
Li, Guoliang <kxmd2008@gmail.com>
2014-07-18 17:36:11 +0800

    udpate

--- src/main/java/org/luis/sainteclaires/admin/rest/FileUpload.java
+++ src/main/java/org/luis/sainteclaires/admin/rest/FileUpload.java
@@ -4,8 +4,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+import org.apache.log4j.Logger;
 import org.luis.basic.util.BasicUtil;
 import org.luis.basic.util.FileUtil;
+import org.luis.basic.util.ImageUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,23 +16,28 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileUpload {
-
+	private static final Logger logger = Logger.getLogger(FileUpload.class);
 	@ResponseBody
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public String upload(String fileName, MultipartFile imgFile) {
+	public String upload(String fileName, MultipartFile ufile) {
 		try {
-			if (!imgFile.isEmpty()) {
-				//文件上传路径
+			if (!ufile.isEmpty()) {
+				// 文件上传路径
 				String file = BasicUtil.getWebAppPath() + "product/imgs/"
-						+ imgFile.getOriginalFilename();
+						+ ufile.getOriginalFilename();
+				logger.info("上传文件：" + file);
 				FileUtil.mkdirs(file);
-				byte[] bytes = imgFile.getBytes();
-				FileOutputStream fos = new FileOutputStream(file); 
+				byte[] bytes = ufile.getBytes();
+				FileOutputStream fos = new FileOutputStream(file);
 				fos.write(bytes); // 写入文件
 				fos.close();
+				logger.info("上传文件成功！");
+				String previewFile = BasicUtil.getWebAppPath()
+						+ "product/preview/" + ufile.getOriginalFilename();
+				ImageUtil.createPreviewImage(file, previewFile);
+				logger.info("生成缩略图"+previewFile);
 			}
-			String string = new String(imgFile.getBytes(), "UTF-8");
-			System.out.println(string);
+
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
